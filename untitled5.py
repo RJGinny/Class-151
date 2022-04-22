e25d5e614bb795a2e7fbef21cd103df4aaa7a29d
RJGinny <92090042+RJGinny@users.noreply.github.com>
2022-04-22 15:41:13 +0530

    Add files via upload

---
+++ untitled5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 15:18:40 2022
+
+@author: riddhiekajain
+"""
+
+month = ("January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+profits = (20000, 45000, 78000, 97000, 12000, 456000,65000, 54000, 9990, 8856, 300, 69000  )
+
+
+max_profit = max(profits)
+max_profit_index = profits.index(max_profit)
+print(max_profit_index)
+
+max_profit_month = month[max_profit_index]
+print("The maximum profit of " + str(max_profit)+ "was recorded in the month of" + str(max_profit_month))
+
+min_profit = min(profits)
+min_profit_index = profits.index(min_profit)
+print(min_profit_index)
+
+min_profit_month = month[min_profit_index]
+print("The minimum profit of "+str(min_profit)+"was recorded in the month of "+str(min_profit_month))
+
